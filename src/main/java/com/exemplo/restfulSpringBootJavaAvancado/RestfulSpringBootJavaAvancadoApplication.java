b2fd20a77ec5c18e0fcdf7c3662b109ba07aceb1
oficina01 <oficina01@viacaograciosa.intranet>
2018-10-16 14:10:58 -0300

    arquivos iniciais do projeto

---
+++ src/main/java/com/exemplo/restfulSpringBootJavaAvancado/RestfulSpringBootJavaAvancadoApplication.java
@@ -0,0 +1,12 @@
+package com.exemplo.restfulSpringBootJavaAvancado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulSpringBootJavaAvancadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulSpringBootJavaAvancadoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/exemplo/restfulSpringBootJavaAvancado/RestfulSpringBootJavaAvancadoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.exemplo.restfulSpringBootJavaAvancado;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulSpringBootJavaAvancadoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
